9069ca9a5f8dd00f38873a86f6e9c3dc9c9ceda2
John H. Sheridan <john.hunter.sheridan@gmail.com>
2013-04-09 21:30:45 -0500

    adding the rest of the post types

--- simpleblog/forms.py
+++ simpleblog/forms.py
@@ -1,23 +1,48 @@
 from flask.ext.wtf import Form, TextField, TextAreaField, validators, html5, FileField
 
 class NewTextBlogForm(Form):
-	title = TextField('Title', [
+	title 	= TextField('Title', [
 		validators.Required()
 	])
-	tags = TextField('tags')
-	body = TextAreaField('Body')
+	tags 	= TextField('tags')
+	body 	= TextAreaField('Body')
 
 class NewPhotoBlogForm(Form):
-	title = TextField('Title', [
+	title 		= TextField('Title', [
 		validators.Required()
 	])	
-	tags = TextField('tags')	
-	photo_link = html5.URLField('link')
-	photo_file = FileField('file')
+	tags 		= TextField('tags')	
+	photo_link  = html5.URLField('link')
+	photo_file 	= FileField('file')
 
 class NewQuoteBlogForm(Form):
-	title = TextField('Title', [
+	title 	= TextField('Title', [
 		validators.Required()
 	])
-	tags = TextField('tags')
-	body = TextAreaField('body')
+	tags 	= TextField('tags')
+	body 	= TextAreaField('body')
+	source 	= TextField('source')
+
+class NewLinkBlogForm(Form):
+	title 	= TextField('Title', [
+		validators.Required()
+	])
+	tags 	= TextField('tags')
+	link 	= TextField('link')
+	body 	= TextAreaField('body')
+
+class NewVideoBlogForm(Form):
+	title 	= TextField('Title', [
+		validators.Required()
+	])
+	tags 	= TextField('tags')
+	link 	= TextField('link')
+	body 	= TextAreaField('body')
+
+class NewAudioBlogForm(Form):
+	title 	= TextField('Title', [
+		validators.Required()
+	])
+	tags 	= TextField('tags')
+	link 	= TextField('link')
+	body 	= TextAreaField('body')
