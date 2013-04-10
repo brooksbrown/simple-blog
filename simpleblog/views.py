bf15d04555453f92047c1415f58c0d8e9c0a8cff
Brooks Brown <brooks.david.brown@gmail.com>
2013-04-09 23:52:33 -0400

    starting object creation code

--- simpleblog/views.py
+++ simpleblog/views.py
@@ -1,9 +1,10 @@
 import random, string
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from database import db
 
 from forms import NewTextBlogForm, NewPhotoBlogForm, NewQuoteBlogForm, NewVideoBlogForm, NewAudioBlogForm, NewLinkBlogForm
+from models import BlogEntry, BlogEntryText
 
 app = Blueprint('simpleblog',
 				__name__, 
@@ -14,6 +15,22 @@ app = Blueprint('simpleblog',
 @app.route('/text/new')
 def new_text_blog():
 	form = NewTextBlogForm()
+
+	if request.method == 'POST' and form.validate():
+
+		new_blog = BlogEntry()
+		new_blog.title = form.title.data
+	
+		db.session.add(new_blog)
+		db.session.commit()
+
+		new_blog_data = BlogEntryText()
+		new_blog_data.blog_entry_id = new_blog.id
+		new_blog_data.body = form.body.data
+
+		db.session.add(new_blog_data)
+		db.session.commit()
+
 	return render_template('simpleblog/text-new.html', form=form)	
 
 @app.route('/photo/new')
